6f222b270b3252e6fe528fcad19d99acdb89506d
Volodymyr1973 <vlrohmanov@gmail.com>
2022-09-23 12:24:57 +0300

    1 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+// const markup = galleryItems.reduce(
+//   (acc, item) =>
+//     acc +
+//     `<div class="gallery__item">
+//   <a class="gallery__link" href="${item.original}">
+//     <img
+//       class="gallery__image"
+//       src="${item.preview}"
+//       data-source="${item.original}"
+//       alt="${item.description}"
+//     />
+//   </a>
+// </div>`,
+//   ""
+// );
+
+// galleryEl.insertAdjacentHTML("beforeend", markup);
+
+const markup = galleryItems
+  .map(
+    (item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+galleryEl.innerHTML = markup;
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+}
